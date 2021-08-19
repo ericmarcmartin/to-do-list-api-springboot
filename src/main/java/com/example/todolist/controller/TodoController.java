2c4dec5dd4e640e06cc0e1aad20d8d4f0b669ffc
martier <martier@oocl.com>
2021-08-19 14:14:53 +0800

    [APISpringBootTodoList][Eric] fix: add @Autowired

--- src/main/java/com/example/todolist/controller/TodoController.java
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -2,6 +2,7 @@ package com.example.todolist.controller;
 
 import com.example.todolist.model.Todo;
 import com.example.todolist.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins ={"http://localhost:3000/", "http://localhost:8090/"})
+@CrossOrigin(origins ={"http://localhost:3001/", "http://localhost:8090/"})
 @RestController
 @RequestMapping("/todos")
 public class TodoController {
+    @Autowired
     private final TodoService todoService;
 
     public TodoController(TodoService todoService) {
--- src/main/java/com/example/todolist/service/TodoService.java
+++ src/main/java/com/example/todolist/service/TodoService.java
@@ -2,12 +2,14 @@ package com.example.todolist.service;
 
 import com.example.todolist.model.Todo;
 import com.example.todolist.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TodoService {
+    @Autowired
     TodoRepository todoRepository;
     public List<Todo> getAll() {
        return todoRepository.findAll();
