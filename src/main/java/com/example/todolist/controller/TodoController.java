399ea07d0e2a617bd1efcda79acc28eb1811870f
martier <martier@oocl.com>
2021-08-19 14:02:05 +0800

    [APISpringBootTodoList][Eric] feat: add CorsGlobalConfiguration

--- src/main/java/com/example/todolist/controller/TodoController.java
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -2,12 +2,14 @@ package com.example.todolist.controller;
 
 import com.example.todolist.model.Todo;
 import com.example.todolist.service.TodoService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins ={"http://localhost:3000/", "http://localhost:8090/"})
 @RestController
 @RequestMapping("/todos")
 public class TodoController {
@@ -17,10 +19,9 @@ public class TodoController {
         this.todoService = todoService;
     }
 
+    @CrossOrigin
     @GetMapping
     public List<Todo> getTodos() {
         return todoService.getAll();
     }
-
-
 }
